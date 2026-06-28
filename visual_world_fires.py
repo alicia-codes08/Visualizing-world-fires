d3f6c8e102d3bcd5378afa26d41b957d47b84546
Alicia <Alicia@sigma.com>
2026-06-28 13:07:46 +0200

    Finished first visual

--- visual_world_fires.py
+++ visual_world_fires.py
@@ -1,5 +1,6 @@
 from pathlib import Path 
 import csv 
+import plotly.express as px
 
 path = Path("fire_data/world_fires_day.csv")
 lines = path.read_text(encoding="utf-8").splitlines()
@@ -11,8 +12,15 @@ for index, element in enumerate(header_row):
     print(index, element) 
 
 # extract data 
-lons = [lon[1] for lon in reader]
-lats = [lat[0] for lat in reader]
-brightness = [bright[2] for bright in reader]
+lons, lats, brightness = [], [], []
+for row in reader:
+    lons.append(float(row[1]))
+    lats.append(float(row[0]))
+    brightness.append(float(row[2]))
 
 # visualize data
+title = "World fires 2022-04-03" 
+fig = px.scatter_geo(lon=lons, lat=lats, size=brightness, color=brightness,
+                    color_continuous_scale='sunset', projection="natural earth",
+                    labels={"color":"brightness"}, title=title)
+fig.show() 
