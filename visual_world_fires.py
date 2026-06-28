540db29e6d42998bb8c118b079cea37252189105
Alicia <Alicia@sigma.com>
2026-06-28 12:37:54 +0200

    Took another dataset

--- visual_world_fires.py
+++ visual_world_fires.py
@@ -1,2 +1,18 @@
 from pathlib import Path 
 import csv 
+
+path = Path("fire_data/world_fires_day.csv")
+lines = path.read_text(encoding="utf-8").splitlines()
+reader = csv.reader(lines)
+header_row = next(reader)
+
+# get index of the information
+for index, element in enumerate(header_row):
+    print(index, element) 
+
+# extract data 
+lons = [lon[1] for lon in reader]
+lats = [lat[0] for lat in reader]
+brightness = [bright[2] for bright in reader]
+
+# visualize data
