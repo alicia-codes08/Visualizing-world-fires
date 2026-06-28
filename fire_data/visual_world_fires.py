d5af0bde444f1bc78483027597e37939370a3887
Alicia <Alicia@sigma.com>
2026-06-28 15:03:32 +0200

    Extracted data

--- fire_data/visual_world_fires.py
+++ fire_data/visual_world_fires.py
@@ -1,10 +1,28 @@
 import csv 
 from pathlib import Path
+from collections import Counter
 
 path = Path("fire_data/world_fires.csv")
 lines = path.read_text(encoding="utf-8").splitlines()
 reader = csv.reader(lines, delimiter=";")
 header_row = next(reader)
 
+# getting index of the elements
 for index, element in enumerate(header_row):
     print(index, element) 
+
+# extract data
+countries_full = []
+for row in reader:
+    country = row[3]
+    countries_full.append(country)
+
+counts = Counter(countries_full)
+countries = []
+numbers = []
+for country, number in counts.items():
+    countries.append(country)
+    numbers.append(number)
+
+print(countries)
+print(numbers)
